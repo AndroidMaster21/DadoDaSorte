c58bdd28c9d6632de0d2d0aac911837aae1a642d
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-04-03 12:08:35 -0300

    Add comments

--- app/src/main/java/br/com/androidmaster/dadodasorte/MainActivity.kt
+++ app/src/main/java/br/com/androidmaster/dadodasorte/MainActivity.kt
@@ -3,11 +3,31 @@ package br.com.androidmaster.dadodasorte
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import br.com.androidmaster.dadodasorte.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    // TODO 1: Declarar a variável do ViewBinding
+
+    // TODO 2: Definir a lista de todas as faces do dado
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // TODO 3: Inicializar o ViewBinding
+
+        // TODO 3: Definir a view da Activity
+        setContentView()
+
+        // TODO 3: Detectar o pressionamento do botão
+        binding.button.setOnClickListener {
+
+            // TODO 5: Gerar um número aleatório de 0 a 6
+
+            // TODO 6: Obter o Drawable da face do dado usando o número aleatório
+
+            // TODO 7: Definir o ImageDrawable no ImageView
+        }
     }
 }
